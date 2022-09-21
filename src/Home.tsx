9128305844d3b335ca557f1af9472820b0bcb6e9
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-21 15:31:45 -0500

    Add links to home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Home.css";
 
 function Home(): JSX.Element {
   return (
-    <h2>
-      This vending machine can sell you dried squid, pickled okra, and hominy.
-      Enjoy!
-    </h2>
+    <>
+      <h2>
+        This vending machine sells dried squid, pickled okra, and the best
+        hominy you ever had. Enjoy!
+      </h2>
+      <ul className="Home-item-list">
+        <li>
+          <Link to="squid">Dried Squid</Link>
+        </li>
+        <li>
+          <Link to="okra">Pickled Okra</Link>
+        </li>
+        <li>
+          <Link to="hominy">Hominy</Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
